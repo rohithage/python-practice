0e3feb8d1a4f364854806a8093aeb2b2dbff3b58
rohithage <115352461+rohithage@users.noreply.github.com>
2025-05-05 13:18:39 +0530

    Add files via upload

---
+++ Loops/challenge_2.py
@@ -0,0 +1,3 @@
+# Loop 5 times (i = 0, 1, 2, 3, 4)
+for i in range(5):
+    print("Hello") # Print Hello in each iteration
---
+++ Loops/challenge_4.py
@@ -0,0 +1,3 @@
+# Loop from 2 to 20, stepping +2 to get only even numbers
+for i in range(2, 21, 2):
+    print(i) # Print the current even number
