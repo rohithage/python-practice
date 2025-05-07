8036fa11b9cb18c28071748c1700385153924cb7
rohithage <115352461+rohithage@users.noreply.github.com>
2025-05-07 11:12:25 +0530

    Delete Loops/Mini Challenges directory

--- Loops/Mini Challenges/challenge_4.py
+++
@@ -1,3 +0,0 @@
-# Loop from 2 to 20, stepping +2 to get only even numbers
-for i in range(2, 21, 2):
-    print(i) # Print the current even number
--- Loops/Mini Challenges/challenge_6.py
+++
@@ -1,6 +0,0 @@
-# Start a for loop
-# i starts from 5, goes up to (and including) 50, incrementing by 5 each time
-for i in range(5, 51, +5):
-
-    # Print the current multiple of 5
-    print(i)
